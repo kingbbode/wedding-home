e75b2b1c5e95cd62d39288d1537863f5c62830c6
권용근 <kingbbode@gmail.com>
2017-06-05 00:31:32 +0900

    chat enable production mode

--- src/main/resources/static/angular/chat.main.ts
+++ src/main/resources/static/angular/chat.main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ChatModule } from './chat.module';
+import {enableProdMode} from '@angular/core';
 
+if (!/localhost/.test(document.location.host)) {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(ChatModule);
