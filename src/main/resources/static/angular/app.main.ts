1f983efb373918088124700efd3de11c4dc18974
권용근 <kingbbode@gmail.com>
2017-06-05 00:03:34 +0900

    angular2 enable production mode

--- src/main/resources/static/angular/app.main.ts
+++ src/main/resources/static/angular/app.main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
-import { AppModule } from './app.module';  
+import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
 
+if (!/localhost/.test(document.location.host)) {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
